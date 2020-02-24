e0918d147d21279a9796b2e1b8bad5b18780543a
etrzupek23 <57397879+etrzupek23@users.noreply.github.com>
2020-02-24 15:18:06 -0500

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -6,13 +6,18 @@ Intermediate Programming - Block 8
 */
 class FizzBuzz {
   public static void main(String[] args) {
+    //for loop
     for (int i = 1; i <= 100; i++){
+      //if the variable i is both multiples of 5 and 3, it prints FizzBuzz
       if (i%3 == 0 && i%5 == 0) {
         System.out.println("FizzBuzz");
+      //if the variable i is a multiple of 3 but not 5, it prints Fizz
       } else if (i%3 == 0 && i%5 != 0) {
         System.out.println("Fizz");
+      //if the variable i is a multiple of 5 but not 3, it prints Buzz
       } else if (i%5 == 0 && i%3 != 0) {
         System.out.println("Buzz");
+      //if the variable i is neither multiple of 3 or 5, it prints the number
       } else {
         System.out.println(i);
       }
